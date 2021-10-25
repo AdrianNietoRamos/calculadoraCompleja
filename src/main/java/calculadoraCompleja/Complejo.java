98f816ae32723bb31808334173ad7645835b165a
AdrianNietoRamos <adrianietoemfb@gmail.com>
2021-10-25 08:54:15 +0100

    Update Complejo.java

--- src/main/java/calculadoraCompleja/Complejo.java
+++ src/main/java/calculadoraCompleja/Complejo.java
@@ -8,8 +8,8 @@ public class Complejo {
 	private DoubleProperty real=new SimpleDoubleProperty();
 	private DoubleProperty imaginario=new SimpleDoubleProperty();
 	
-	public DoubleProperty realProperty() {
-		return this.real;
+	public final DoubleProperty realProperty() {
+		return real;
 	}
 	
 
@@ -53,6 +53,48 @@ public class Complejo {
 		return "("+getReal()+","+getImaginario()+")";
 		
 	}
+	//(a+c, b+d)
+	public Complejo sumar(Complejo suma) {
+		Complejo s=new Complejo();
+		s.realProperty().bind(realProperty().add(suma.realProperty()));
+		s.imaginarioProperty().bind(imaginarioProperty().add(suma.imaginarioProperty()));
+		
+		return s;
+	}
+	//(a-c, b-d)
+public Complejo restar(Complejo resta) {
+	Complejo r=new Complejo();
+	r.realProperty().bind(realProperty().subtract(resta.realProperty()));
+	r.imaginarioProperty().bind(imaginarioProperty().subtract(resta.imaginarioProperty()));
+	
+	return r;
+	}
+//(a*c - b*d, a*d + b*c)
+public Complejo multiplicar(Complejo multi) {
+	Complejo m=new Complejo();
+	m.realProperty().bind(realProperty().multiply(multi.realProperty())
+			.subtract(imaginarioProperty().multiply(multi.imaginarioProperty())));
+	m.imaginarioProperty().bind(realProperty().multiply(multi.imaginarioProperty())
+			.add(imaginarioProperty().multiply(multi.realProperty())));
+	
+	return m;
+}
+//((a*c+b*d)/c*c+d*d),((b*c-a*d)/c*c+d*d)
+public Complejo dividir(Complejo divide) {
+	Complejo d=new Complejo();
+	d.realProperty().bind((realProperty()
+			.multiply(divide.realProperty())
+			.add(imaginarioProperty().multiply(divide.imaginarioProperty())))
+			.divide(divide.realProperty().multiply(divide.realProperty()).add(divide.imaginarioProperty().multiply(divide.imaginarioProperty()))));
+	
+	d.imaginarioProperty().bind((imaginarioProperty()
+			.multiply(divide.realProperty())
+			.subtract(realProperty().multiply(divide.imaginarioProperty())))
+			.divide(divide.realProperty().multiply(divide.realProperty()).add(divide.imaginarioProperty().multiply(divide.imaginarioProperty()))));
+	
+	
+	return d;
+}
 	
 
 
